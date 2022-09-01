000a0e187b2cf8a9912c8817e3906576970c281e
simnga <simeon.c.ngatai@gmail.com>
2022-09-01 20:45:32 +1200

    First Commit

---
+++ gaia_project.R
@@ -0,0 +1,16 @@
+# Import gaia data
+data <- read.csv("gaia.csv")
+
+# Total missing data
+sum(is.na(data))
+
+# Count data type
+table(sapply(data, class))
+
+# EDA - Simeon
+
+
+# EDA  - Shaam
+
+
+# EDA - Tama
