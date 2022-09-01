f99e9167e0872c6cbb0f866eb6d6580b3748561c
simnga <simeon.c.ngatai@gmail.com>
2022-09-01 21:35:52 +1200

    Update

--- gaia_project.R
+++ gaia_project.R
@@ -1,4 +1,4 @@
-# Import gaia data
+# Import GAIA data (GaiaSource_1000172165251650944_1000424567594791808)
 data <- read.csv("gaia.csv")
 
 # Total missing data
@@ -6,6 +6,12 @@ sum(is.na(data))
 
 # Count data type
 table(sapply(data, class))
+
+# % of missing data.
+sum(is.na(data))/prod(dim(data)) * 100
+
+# Columns that contain missing data
+names(which(sapply(data, anyNA)))
 
 # EDA - Simeon
 
